9fe3c110548cd1dfada04d59ab9aa7a82709b756
Pazaitis <pazaitis@web.de>
2023-02-26 16:46:49 +0100

    push

--- api/subscribe/__init__.py
+++ api/subscribe/__init__.py
@@ -10,23 +10,23 @@ import azure.functions as func
 def main(req: func.HttpRequest, outputTable: func.Out[str]) -> func.HttpResponse:
     logging.info('createPage HTTP trigger function processed a request.')
 
-    url_gmaps = "http://localhost:7071/api/gmaps?inputprompt=test"
+    url_gmaps = "https://polite-ocean-0507ce403.2.azurestaticapps.net/api/gmaps?inputprompt=test"
     gmaps_request = requests.request("POST", url_gmaps)
+    gmaps_request_res = gmaps_request.text
     input = req.params.get('inputprompt')
 
     rowKey = str(uuid.uuid4())
     # res = "start"
     errorAI = ""
 
-    # api_key = os.environ['openAIKey']
-    api_key = "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"
+    api_key = os.environ['openAIKey']
     if input:
         
         try:
             openai.api_key = api_key
             response = openai.Completion.create(
             model="text-davinci-003",
-            prompt="Schreibe ein Expose für eine Wohnung mit folgenden Daten. Unterteile dabei das Ergebnis in Ausstattung (100 Wörter) und Lagebeschreibung (100 Wörter) auf und geben das Ergebnis in diesem json Format aus: result: Ausstattung: , Lage: "+input,
+            prompt="Schreibe ein Expose für eine Wohnung mit folgenden Daten. Unterteile dabei das Ergebnis in Ausstattung (100 Wörter) und Lagebeschreibung (100 Wörter) auf und geben das Ergebnis in diesem json Format aus: result: Ausstattung: , Lage: "+input +". Nutze für die Lagebeschreibung folgende Haltestellen für öffentliche Verkehrsmittel:" + gmaps_request_res ,
             temperature=0.7,
             max_tokens=1000,
             top_p=1,
